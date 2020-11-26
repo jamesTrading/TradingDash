b8de1dd16073f008289eefdafd0330d7bffd9b01
jamesTrading <65225964+jamesTrading@users.noreply.github.com>
2020-11-26 20:44:12 +1100

    Update app.py

--- app.py
+++ app.py
@@ -447,7 +447,7 @@ def TradingAlgo(selected_dropdown_value, junky, signalinput):
     TYPValue = TYPValue / (days)
     outputlist.append(("The last buy date is: ", dates[len(dates)-1]))
     outputlist.append(("The last price bought for: $", homie[len(homie)-1]))
-    outputlist.append(("The MFI of the last buy: ", MFItracker[len(MFItracker)-1]))
+    outputlist.append(("The MFI of the last buy: ", round(MFItracker[len(MFItracker)-1],2)))
     outputlist.append(("The expected sell point is: $", round((lastmax[len(lastmax)-1]-lastmin[len(lastmax)-1])*0.618+homie[len(homie)-1],2)))
     outputlist.append(("The expected return on the buy: ", round(((((lastmax[len(lastmax)-1]-lastmin[len(lastmax)-1])*0.618+homie[len(homie)-1]-homie[len(homie)-1])/homie[len(homie)-1])*100),3),"%"))
     outputlist.append(("The average return on the each buy is: ", round((sum(tendies)/len(tendies))*100,3),"%"))
